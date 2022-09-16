bb28d9c9453a1f8a0af5578de9a6a0ae7f2071da
Best Cat <kotova-irina11@rambler.ru>
2022-09-16 23:40:11 +0300

    занятие 5

---
+++ ex_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника 
+//меньше суммы двух других сторон.
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите тертье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int TrangleMethod (int number1, int number2, int number3)
+{
+if(number2 < number1 + number3);
+{
+if(number3 < number1 + number2);
+{
+if (number3 < number1 + number2);
+{
+Console.WriteLine ($"да");
+}
+else
+{
+Console.WriteLine ($"нет");   
+}
+}
+return number1;
+}
+}
+TrangleMethod(num1,num2,num3);
---
+++ ex_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+Console.WriteLine("Веедите перdое число: ");
+int num1 = int.Parse(Console.ReadLine());
+int Method(int number)
+{
+    if(number % 2 != 0)
+    {
+    number = number % 2;
+    Console.WriteLine ($"{number}");
+    }
+return number;
+}
+Method(number);
+
