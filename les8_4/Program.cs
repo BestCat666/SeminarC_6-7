3824261fc79e2506da45a086e0218578a47ffcd8
Best Cat <kotova-irina11@rambler.ru>
2022-09-27 23:10:14 +0300

    задача 67, рекурсия сумма цифр в числе

---
+++ les8_4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+int Sum(int n)
+{
+if (n == 0) return 0;
+else return n % 10 + Sum (n/10);
+}
+Console.WriteLine(Sum(a));
