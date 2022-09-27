e08ebeda301021b6707ad00ed643f562747b606a
Best Cat <kotova-irina11@rambler.ru>
2022-09-27 23:08:53 +0300

    задача 65, рекурсия натуральные числа

---
+++ les8_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 6, 7, 8"
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+string Num(int n, int a)
+{
+ if( n == a ) return $"{Convert.ToString(a)}";
+ else return Num(n - 1,a) + n;
+}
+Console.WriteLine(Num(b, a));
